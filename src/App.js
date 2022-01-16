dae8d8f9e35497c2d38636dc3e13b9d245c573f7
Huann Almeida <huannvictor@gmail.com>
2022-01-16 15:38:25 -0300

    levels component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+import levels from "./components/Levels";
+
 const App = () => {
   const [chosenLevel, setChosenLevel] = useState(null);
   const [words, setWords] = useState(null);
@@ -47,53 +49,6 @@ const App = () => {
   console.log("correctAnswers", correctAnswers);
   console.log("clicked", clicked);
 
-  const options = [
-    {
-      select: "Select a level",
-      value: null,
-    },
-    {
-      select: "level 01",
-      value: 1,
-    },
-    {
-      select: "level 02",
-      value: 2,
-    },
-    {
-      select: "level 03",
-      value: 3,
-    },
-    {
-      select: "level 04",
-      value: 4,
-    },
-    {
-      select: "level 05",
-      value: 5,
-    },
-    {
-      select: "level 06",
-      value: 6,
-    },
-    {
-      select: "level 07",
-      value: 7,
-    },
-    {
-      select: "level 08",
-      value: 8,
-    },
-    {
-      select: "level 09",
-      value: 9,
-    },
-    {
-      select: "level 10",
-      value: 10,
-    },
-  ];
-
   return (
     <div className="app">
       {!chosenLevel && (
@@ -106,7 +61,7 @@ const App = () => {
             value={chosenLevel}
             onChange={e => setChosenLevel(e.target.value)}
           >
-            {options.map((option, _index) => (
+            {levels.map((option, _index) => (
               <option key={_index} value={option.value}>
                 {option.select}
               </option>
---
+++ src/components/Levels.js
@@ -0,0 +1,48 @@
+const levels = [
+  {
+    select: "Select a level",
+    value: null,
+  },
+  {
+    select: "level 01",
+    value: 1,
+  },
+  {
+    select: "level 02",
+    value: 2,
+  },
+  {
+    select: "level 03",
+    value: 3,
+  },
+  {
+    select: "level 04",
+    value: 4,
+  },
+  {
+    select: "level 05",
+    value: 5,
+  },
+  {
+    select: "level 06",
+    value: 6,
+  },
+  {
+    select: "level 07",
+    value: 7,
+  },
+  {
+    select: "level 08",
+    value: 8,
+  },
+  {
+    select: "level 09",
+    value: 9,
+  },
+  {
+    select: "level 10",
+    value: 10,
+  },
+];
+
+export default levels;
